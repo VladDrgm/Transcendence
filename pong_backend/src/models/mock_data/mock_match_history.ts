2103e797881c722b8f7ee98a11a1c4b2bf0757e1
LidiaOrfanu <lidia3991@yahoo.com>
2023-04-23 13:29:48 +0200

    add mock data

---
+++ pong_backend/src/models/mock_data/mock_match_history.ts
@@ -0,0 +1,27 @@
+export const matchesHistory = [
+  {
+    matchHistoryId: 1,
+    matchId: 1,
+    userId: 1,
+  },
+  {
+    matchHistoryId: 2,
+    matchId: 2,
+    userId: 3,
+  },
+  {
+    matchHistoryId: 3,
+    matchId: 3,
+    userId: 4,
+  },
+  {
+    matchHistoryId: 4,
+    matchId: 4,
+    userId: 2,
+  },
+  {
+    matchHistoryId: 5,
+    matchId: 5,
+    userId: 5,
+  },
+];
