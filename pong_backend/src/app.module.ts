456caac4906064863bd69233103e2df921b21ec5
VladDrag <vddragomi@icloud.com>
2023-06-06 08:15:15 +0200

    Add Match and MatchHistory according to Database Schematic + Add database schematic txt file in orm_models

--- pong_backend/src/app.module.ts
+++ pong_backend/src/app.module.ts
@@ -14,9 +14,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './models/orm_models/user.entity';
 import { Blocked } from './models/orm_models/blocked.entity';
 import { Friend } from './models/orm_models/friend.entity';
+import { Match } from './models/orm_models/match.entity';
+import { MatchHistory } from './models/orm_models/matchHistory.entity';
 
 @Module({
-  imports: [DatabaseModule, TypeOrmModule.forFeature([User, Blocked, Friend])],
+  imports: [DatabaseModule, TypeOrmModule.forFeature([User, Blocked, Friend, Match, MatchHistory])],
   controllers: [AppController, HelloController],
   providers: [AppService],
 })
---
+++ pong_backend/src/models/orm_models/match.entity.ts
@@ -0,0 +1,33 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { User } from './user.entity';
+import { MatchHistory } from './matchHistory.entity';
+
+@Entity()
+export class Match {
+  @PrimaryGeneratedColumn()
+  MatchId: number;
+
+  @ManyToOne(() => MatchHistory, (matchHistory) => matchHistory.matches)
+  matchHistory: MatchHistory;
+
+  @ManyToOne(() => User)
+  UserId: User;
+
+  @ManyToOne(() => User)
+  OpponentId: User;
+
+  @Column()
+  Player1Points: number;
+
+  @Column()
+  Player2Points: number;
+
+  @Column()
+  GameType: string;
+
+  @Column()
+  FinalResultString: string;
+
+  @Column()
+  Date: Date;
+}
