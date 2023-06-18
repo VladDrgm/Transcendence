8b1d8d3264368db22a4a2988fcac7f4d782e73ce
Tim <timygerdes@gmail.com>
2023-06-18 20:45:33 +0200

    Login page design

--- pong_frontend/src/components/LogIn.tsx
+++ pong_frontend/src/components/LogIn.tsx
@@ -1,6 +1,7 @@
 import React, {useRef} from 'react';
 import { login } from '../api/login.api';
 import CSS from 'csstype';
+import gif from './assets/billy.png'
 
 interface LogInProps
 {
@@ -10,89 +11,73 @@ interface LogInProps
 
 const LogIn: React.FC<LogInProps> = ({userID_set, loginDone_set}) => {
   const input_id = useRef<HTMLInputElement>(null);
+  var input_user_id;
 
 const pageStyle: CSS.Properties = {
-	backgroundColor: 'rgba(150, 0, 0, 1)',
+	backgroundColor: 'rgba(3, 3, 3, 1)',
 	height: '100%',
 	width: '100%',
 	position: 'absolute',
+	alignItems: 'center',
+	flexDirection: 'column',
+	textAlign: 'center',
+	borderColor: 'green',
+	borderWidth: '5px',
 }
 
+const gifStyle: CSS.Properties = {
+	position: 'relative',
+	textAlign: 'center',
+	top: '5rem',
+	padding: '1.5rem',
+	fontFamily: 'sans-serif',
+	fontSize: '1.5rem',
+	width:'360px',
+	height:'360px', 
+  //   boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)'
+  };
+
 const welcomeTitleStyle: CSS.Properties = {
-  backgroundColor: 'rgba(0, 0, 0, 0.6)',
-  color: 'rgba(255, 255, 255, 1)',
+  color: 'rgba(254, 8, 16, 1)',
   position: 'relative',
   textAlign: 'center',
   top: '2rem',
   padding: '1.5rem',
-  fontFamily: 'sans-serif',
-  fontSize: '1.5rem',
-//   boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)'
+  fontFamily: 'Shlop',
+  fontSize: '5rem',
 };
 
-const loginTitleStyle: CSS.Properties = {
-	color: 'rgba(255, 255, 255, 1)',
-	position: 'relative',
-	textAlign: 'center',
-	top: '2rem',
-	fontFamily: 'sans-serif',
-	fontSize: '1rem',
-}
-
-const idNumberTitleStyle: CSS.Properties = {
-	color: 'rgba(255, 255, 255, 1)',
-	position: 'relative',
-	textAlign: 'center',
-	top: '2rem',
-	fontFamily: 'sans-serif',
-	fontSize: '1rem',
-}
-
-const textFieldStyle: CSS.Properties = {
-	position: 'relative',
-	textAlign: 'center',
-	top: '2rem',
-	fontFamily: 'sans-serif',
-	fontSize: '1rem',
-}
-
 const loginButtonStyle: CSS.Properties = {
+	backgroundColor: 'rgba(254, 8, 16, 1)',
 	position: 'relative',
-	textAlign: 'center',
-	top: '2rem',
-	fontFamily: 'sans-serif',
-	fontSize: '1rem',
-}
-
-const registerButtonStyle: CSS.Properties = {
-	position: 'relative',
-	textAlign: 'center',
-	top: '2rem',
-	fontFamily: 'sans-serif',
-	fontSize: '1rem',
+	height:'40px',
+	width:'160px',
+	top: '4rem',
+	fontFamily: 'Shlop',
+	fontSize: '1.5rem',
+	alignSelf: 'center',
+	borderRadius: '6px',
+	border: 'none',
+	color:'white',
 }
 
   async function saveUser_id() {
-    if (input_id.current != null)
+	input_user_id = 1
+	if (input_user_id != 0)
     {
-      const parsedInput = parseInt(input_id.current.value);
-      if (!isNaN(parsedInput))
+      if (!isNaN(input_user_id))
       {
-        await login(parsedInput);
-        userID_set(parseInt(input_id.current.value));
+        await login(input_user_id);
+        userID_set(input_user_id);
         loginDone_set(true);
       }
-      input_id.current.value = '';
+      input_user_id = 0
     }
   };
   return (<div style={pageStyle}>
+			<img src={gif} style={gifStyle} ></img>
             <p style={welcomeTitleStyle}>Do you want to play a game?</p>
-            <p style={loginTitleStyle}>Login or create user.</p>
-            <div style={idNumberTitleStyle}>Id number:</div>
-            <input style={textFieldStyle} ref={input_id} type="text" />
-            <button style={loginButtonStyle} onClick={saveUser_id}>Log In</button>
-            <br />
-            <button style={registerButtonStyle}>Register</button>
+            <button style={loginButtonStyle} onClick={saveUser_id}>Login with 42</button>
           </div>);
 };
 
