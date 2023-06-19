74510f247502f5b962903e7cc3727ee134595670
ksura12 <private-mail@privat.de>
2023-06-19 12:13:47 +0200

    simple chat dummy

--- pong_frontend/src/components/MainDivSelector.tsx
+++ pong_frontend/src/components/MainDivSelector.tsx
@@ -3,6 +3,8 @@ import Error_MainDiv from './main_div/Error_MainDiv';
 import Welcome_MainDiv from './main_div/Welcome_MainDiv';
 import Profile_MainDiv from './main_div/Profile_MainDiv';
 import Friend_Profile_MainDiv from './main_div/Friend_Profile_MainDiv';
+// import Chat_MainDiv from './main_div/Chat_MainDiv';
+import Arena_Chat_MainDiv from './main_div/Arena_Chat';
 
 
 export enum main_div_mode_t {
@@ -32,6 +34,8 @@ const MainDivSelector: React.FC<MainDivProps> = ({userID, mode, mode_set}) => {
       return (<Profile_MainDiv userID={userID} mode_set={mode_set} />);
     case main_div_mode_t.FRIIEND_PROFILE:
       return (<Friend_Profile_MainDiv userID={userID} mode_set={mode_set} />);
+    case main_div_mode_t.CHAT:
+      return (<Arena_Chat_MainDiv/>)
     default:
       mode_set(main_div_mode_t.ERROR_PAGE);
       return (<Error_MainDiv />);
