54f7a546a5f21646671c5ce8e03855daa3bf6da3
Domen Banfi <banfidomen@gmail.com>
2023-07-02 22:35:09 +0200

    Initial commit

---
+++ pong_backend/src/modules/password/password.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class PasswordService {
+	async hashPassword(password: string): Promise<string> {
+		const hashedPassword = await bcrypt.hash(password, 10);
+		return hashedPassword;
+	}
+
+	async comparePassword(hashedPassword: string, password: string): Promise<boolean> {
+		if (hashedPassword === await bcrypt.hash(password, 10))
+		{
+			return (true);
+		}
+		else
+		{
+			return (false);
+		}
+	}
+}
