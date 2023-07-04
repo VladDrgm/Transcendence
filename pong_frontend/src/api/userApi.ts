34da7c1f7f406289fe645a8716a3ad228908c852
Tim <timygerdes@gmail.com>
2023-07-04 21:39:30 +0200

    Some changes for the main layout

--- pong_frontend/src/api/userApi.ts
+++ pong_frontend/src/api/userApi.ts
@@ -1,5 +1,15 @@
+var fetchAddress = 'http://localhost:3000/';
+
 export const getUsers = async () => {
-  const response = await fetch('http://localhost:3000/api/users');
-  const json = (await response.json());
-  return json;
+  	const response = await fetch(fetchAddress + 'users');
+  	const json = (await response.json());
+	return json;
+};
+
+export const createUser = async () => {
+	const response = await fetch(fetchAddress + 'users');
+	console.log(response.text);
+
+	const json = (await response.json());
+	return json;
 };
--- pong_frontend/src/components/MainDivSelector.tsx
+++ pong_frontend/src/components/MainDivSelector.tsx
@@ -6,41 +6,43 @@ import Friend_Profile_MainDiv from './main_div/Friend_Profile_MainDiv';
 import Leaderboard_MainDiv from './main_div/Leaderboard_MainDiv';
 // import Chat_MainDiv from './main_div/Chat_MainDiv';
 import Arena_Chat_MainDiv from './main_div/Arena_Chat';
-
-
+import Settings_MainDiv from './main_div/Settings_MainDiv';
+import { User } from '../interfaces/user.interface';
 
 export enum main_div_mode_t {
   ERROR_PAGE = -1,
-  WELCOME_PAGE = 0,
-  PROFILE = 1,
-  FRIENDS_LIST = 2, 
-  MATCH_HISTORY = 3,
-  SETTINGS = 4,
-  CHAT = 5,
-  GAME = 6,
-  FRIIEND_PROFILE = 7,
-  LEADERBORAD = 8
+  HOME_PAGE = 0,
+  FRIEND_PROFILE = 1,
+  CHAT = 2,
+  GAME = 3,
+  PROFILE = 4,
+  SETTINGS = 5,
+  LEADERBORAD = 8,
 }
 
 interface MainDivProps
 {
-  userID: number;
+//   userID: number;
+  user: User;
   mode: main_div_mode_t;
   mode_set: React.Dispatch<React.SetStateAction<main_div_mode_t>>;
 }
 
-const MainDivSelector: React.FC<MainDivProps> = ({userID, mode, mode_set}) => {
+const MainDivSelector: React.FC<MainDivProps> = ({/*userID, */mode, mode_set}) => {
+	var userID = 1;
   switch (mode){
-    case main_div_mode_t.WELCOME_PAGE:
-      return (<Welcome_MainDiv userID={userID} />);
-    case main_div_mode_t.PROFILE:
-      return (<Profile_MainDiv userID={userID} mode_set={mode_set} />);
-    case main_div_mode_t.FRIIEND_PROFILE:
-      return (<Friend_Profile_MainDiv userID={userID} mode_set={mode_set} />);
-    case main_div_mode_t.LEADERBORAD:
-      return (<Leaderboard_MainDiv/>);
-    case main_div_mode_t.CHAT:
-      return (<Arena_Chat_MainDiv/>)
+    case main_div_mode_t.HOME_PAGE:
+      return (<Welcome_MainDiv />);
+	case main_div_mode_t.FRIEND_PROFILE:
+		return (<Friend_Profile_MainDiv userID={userID} mode_set={mode_set} />);
+	case main_div_mode_t.CHAT:
+		return (<Arena_Chat_MainDiv/>)
+	case main_div_mode_t.PROFILE:
+		return (<Profile_MainDiv userID={userID} mode_set={mode_set} />);
+	case main_div_mode_t.LEADERBORAD:
+      	return (<Leaderboard_MainDiv/>);
+	case main_div_mode_t.SETTINGS:
+		return (<Settings_MainDiv userID={userID} mode_set={mode_set} />);
     default:
       mode_set(main_div_mode_t.ERROR_PAGE);
       return (<Error_MainDiv />);
