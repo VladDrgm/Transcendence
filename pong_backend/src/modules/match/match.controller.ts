befb40483fd09d29ff1c770faa1596d769cb9fea
Lidia <80785509+LidiaOrfanu@users.noreply.github.com>
2023-07-25 21:24:03 +0200

    update post

--- pong_backend/src/modules/match/match.controller.ts
+++ pong_backend/src/modules/match/match.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body, Get, Delete, Param } from '@nestjs/common';
 import { MatchService } from './match.service';
 import { Match } from 'src/models/orm_models/match.entity';
-import { matchDTO } from './matchDTO';
-import { ApiTags } from '@nestjs/swagger';
+import { MatchDTO } from './matchDTO';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Match')
 @Controller('match')
@@ -10,12 +10,34 @@ export class MatchController {
   constructor(private readonly matchService: MatchService) {}
 
   @Post()
-  async createMatch(@Body() matchDto: matchDTO): Promise<Match> {
-    return this.matchService.createMatch(matchDto);
+  @ApiOperation({ summary: 'Create a match' })
+  async createMatch(@Body() matchDto: MatchDTO,
+  @Param('player1Id') p1Id: number,
+  @Param('player2Id') p2Id: number
+  ): Promise<Match> {
+    return this.matchService.createMatch(matchDto, p1Id, p2Id);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all matches' })
   async getAllMatches(): Promise<Match[]> {
     return this.matchService.getAllMatches();
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get match by id' })
+  async getMatchById(@Param('id') matchId: number): Promise<Match> {
+    return this.matchService.getMatchById(matchId);
+  }
+
+  // @Get('user/:userId')
+  // async getMatchesByUser(@Param('userId') userId: number): Promise<Match[]> {
+  //   return this.matchService.getMatchesByUser(userId);
+  // }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete a match by matchId' })
+  async deleteMatch(@Param('id') matchId: number): Promise<void> {
+    return this.matchService.deleteMatch(matchId);
   }
 }
--- pong_backend/src/modules/match/match.service.ts
+++ pong_backend/src/modules/match/match.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Match } from 'src/models/orm_models/match.entity';
-import { matchDTO } from './matchDTO';
+import { MatchDTO } from './matchDTO';
 import { MatchHistory } from 'src/models/orm_models/matchHistory.entity';
 
 @Injectable()
@@ -10,15 +10,51 @@ export class MatchService {
   constructor(
     @InjectRepository(Match)
     private readonly matchRepository: Repository<Match>,
-    @InjectRepository(MatchHistory)
-    private readonly matchHistoryRepository: Repository<MatchHistory>,
   ) {}
-  async createMatch(matchDto: matchDTO): Promise<Match> {
-    const match = this.matchRepository.create(matchDto);
-    return this.matchRepository.save(match);
+
+  async createMatch(
+    matchDTO: MatchDTO,
+    p1Id: number,
+    p2Id: number
+    ): Promise<Match> {
+    const matchDb = new Match();
+    matchDb.Player1.userID = p1Id;
+    matchDb.Player2.userID = p2Id;
+    matchDb.Player1Points = matchDTO.Player1Points;
+    matchDb.Player2Points = matchDTO.Player2Points;
+    matchDb.GameType = matchDTO.GameType;
+    matchDb.FinalResultString = matchDTO.FinalResultString;
+    matchDb.startTime = matchDTO.startTime;
+    matchDb.endTime = matchDTO.endTime;
+    matchDb.WinnerId = matchDTO.WinnerId;
+    matchDb.WinningCondition = matchDTO.WinningCondition;
+
+    const MatchId = await this.matchRepository
+      .save(matchDb)
+      .then((match) => match.MatchId);
+
+    const player1MatchHistory = new MatchHistory();
+    player1MatchHistory.MatchId = MatchId;
+    player1MatchHistory.UserId.userID = matchDb.Player1.userID;
+    this.matchRepository.save(player1MatchHistory);
+
+    const player2MatchHistory = new MatchHistory();
+    player2MatchHistory.MatchId = MatchId;
+    player2MatchHistory.UserId.userID = matchDb.Player2.userID;
+    this.matchRepository.save(player2MatchHistory);
+
+    return this.matchRepository.save(matchDb);
+  }
+
+  async getMatchById(matchId: number): Promise<Match> {
+    return this.matchRepository.findOneBy({ MatchId: matchId });
   }
 
   async getAllMatches(): Promise<Match[]> {
     return this.matchRepository.find();
+  }
+
+  async deleteMatch(matchId: number): Promise<void> {
+    await this.matchRepository.delete(matchId);
   }
 }
