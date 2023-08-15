2fce23b321ee3865ba39f7930ad63624d2792bd8
Tim <timygerdes@gmail.com>
2023-08-15 22:46:31 +0200

    Fixed branch

--- pong_frontend/src/index.tsx
+++ pong_frontend/src/index.tsx
@@ -1,17 +1,30 @@
+// import './index.css';
+// import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = document.getElementById('root') as HTMLElement;
+// const appRoot = ReactDOM.createRoot(root);
+// appRoot.render(<App />);
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import React from 'react';
-import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-	<React.StrictMode>
-		  <App />
-	</React.StrictMode>,
-	document.getElementById('root')
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
