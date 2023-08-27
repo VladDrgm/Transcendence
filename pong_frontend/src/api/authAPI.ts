d344a6a1fb4821964cced6b58d315a167962be58
Vlad Dragomir <vdragomir@protonmail.com>
2023-08-27 17:08:23 +0200

    Auth + Env

--- pong_frontend/src/api/authAPI.ts
+++ pong_frontend/src/api/authAPI.ts
@@ -54,8 +54,9 @@ export const userLoginAPI = async (username: string, password: string): Promise<
 
 export async function login(userID:number)  { //placeholder for real login
     // const response = await fetch(process.env.REACT_APP_BASE_URL + 'user/login/' + userID, {credentials: 'include',});
-    const response = await fetch('http://localhost:3000/user/login/' + userID, {credentials: 'include',});
+    // const response = await fetch('http://localhost:3000/user/login/' + userID, {credentials: 'include',});
     // const response = await fetch('https://transcendence-server.azurewebsites.net/user/login' + userID, {credentials: 'include',})
+    const response = await fetch(`${process.env.REACT_APP_BASE_URL}` + 'user/login/' + userID, {credentials: 'include',})
     const setCookieHeader = await response.headers.get("Set-Cookie");
     if (setCookieHeader) {
       document.cookie = setCookieHeader;
